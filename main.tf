0ea073b38543c681958fed6cd99a942cd47eddb8
sunillangan <58847457+sunillangan@users.noreply.github.com>
2023-01-27 22:18:34 +0800

    Updated sg

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 
 resource "aws_security_group" "allow_tls" {
-  name = "jenkins-tf-security_group_2"
+  name = "jenkins-tf-security_group_3"
   description = "Allow TLS inbound traffic"
 
   ingress {
